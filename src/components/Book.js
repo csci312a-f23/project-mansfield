6d68fd0ebfdafe0460e8d8b5c245217cd2f1ef0a
Travis Emery <temery@middlebury.edu>
2023-11-05 23:02:50 -0500

    changed dropdown to accomodate different size usernames, added background to login page, added leagues component, added leagues data.

--- src/components/Book.js
+++ src/components/Book.js
@@ -2,20 +2,26 @@ import styles from "@/styles/Book.module.css";
 import { useState } from "react";
 import Board from "./Board";
 import Cart from "./Cart";
+import Leagues from "./Leagues";
+import data from "../../data/selectedsports.json";
 
 export default function Book() {
   // const { currentLeague, setCurrentLeague } = useState();
 
   const [cart, setCart] = useState([]);
+  const [league, setCurrentLeague] = useState(null);
+  const leaguesCollection = data;
 
   return (
     <main className={styles.main}>
       <div className={styles.selector}>
-        <h4>Leagues</h4>
-        heh
+        <Leagues
+          leaguesCollection={leaguesCollection}
+          setCurrentLeague={setCurrentLeague}
+        />
       </div>
       <div className={styles.book}>
-        <Board cart={cart} setCart={setCart} />
+        <Board cart={cart} setCart={setCart} league={league} />
       </div>
       <div className={styles.cart}>
         <Cart cart={cart} setCart={setCart} />
