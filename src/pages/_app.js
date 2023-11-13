bbc93e116fda2c2d4cb44e6688a6c5dc4c5180f2
alexoh2bd <alexoh2020@gmail.com>
2023-11-13 11:52:04 -0500

    fixed router loop

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,20 +5,26 @@ import { useRouter } from "next/router";
 import { initializeApp } from "firebase/app";
 import { firebaseConfig} from "../firebase-config";
 
+
 export default function App({ Component, pageProps }) {
   const router = useRouter();
   const [loggedIn, setLoggedIn] = useState(false);
   const [user, setUser] = useState(null);
   const [currentPending, setCurrentPending] = useState(null);
   initializeApp(firebaseConfig);
-
-  useEffect(() => {
+  
+  const handleRouterPush = () => {
     if (loggedIn) {
       router.push("/");
     } else {
       router.push("/login");
     }
-  }, [loggedIn, router]);
+  };
+  useEffect(() => {
+    // Call the function immediately
+    handleRouterPush();
+  }, [loggedIn]);
+
 
   const props = {
     ...pageProps,
