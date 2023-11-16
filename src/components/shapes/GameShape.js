7c7a4a6c7a5443a10bd76c01d4205d1eee91e87a
Jack Greenburg <jack.greenburg7@gmail.com>
2023-11-15 20:18:08 -0500

    updated game shape

--- src/components/shapes/GameShape.js
+++ src/components/shapes/GameShape.js
@@ -8,35 +8,9 @@
 import PropTypes from "prop-types";
 
 const GameShape = PropTypes.shape({
-  Quarter: PropTypes.string.isRequired,
-  TimeRemaining: PropTypes.string,
-  QuarterDescription: PropTypes.string.isRequired,
-  GameEndDateTime: PropTypes.string.isRequired,
-  AwayScore: PropTypes.number.isRequired,
-  HomeScore: PropTypes.number.isRequired,
-  GameID: PropTypes.number.isRequired,
-  GlobalGameID: PropTypes.number.isRequired,
-  ScoreID: PropTypes.number.isRequired,
-  GameKey: PropTypes.string.isRequired,
-  Season: PropTypes.number.isRequired,
-  SeasonType: PropTypes.number.isRequired,
-  Status: PropTypes.string.isRequired,
-  Canceled: PropTypes.bool.isRequired,
-  Date: PropTypes.string.isRequired,
-  Day: PropTypes.string.isRequired,
-  DateTime: PropTypes.string.isRequired,
-  DateTimeUTC: PropTypes.string.isRequired,
-  AwayTeam: PropTypes.string.isRequired,
-  HomeTeam: PropTypes.string.isRequired,
-  GlobalAwayTeamID: PropTypes.number.isRequired,
-  GlobalHomeTeamID: PropTypes.number.isRequired,
-  AwayTeamID: PropTypes.number.isRequired,
-  HomeTeamID: PropTypes.number.isRequired,
-  StadiumID: PropTypes.number.isRequired,
-  Closed: PropTypes.bool.isRequired,
-  LastUpdated: PropTypes.string.isRequired,
-  IsClosed: PropTypes.bool.isRequired,
-  Week: PropTypes.number.isRequired,
+  commence_time: PropTypes.string.isRequired,
+  home_team: PropTypes.string.isRequired,
+  away_team: PropTypes.string.isRequired,
 });
 
 export default GameShape;
