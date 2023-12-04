d164213905a6357bf4a8487345d7d1f2e6bcb019
Jack Greenburg <jack.greenburg7@gmail.com>
2023-12-04 11:28:13 -0500

    account creation, navbar moved to _app

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,34 @@
 import PropTypes from "prop-types";
 import { useRouter } from "next/router";
-import { signOut } from "next-auth/react";
+import { useEffect } from "react";
+import { useSession, signOut } from "next-auth/react";
 import styles from "../styles/navigation.module.css";
 
-export default function Navbar({ user, balance }) {
-  // const { data: session } = useSession();
+export default function Navbar({ balance, setBalance }) {
+  const { data: session } = useSession();
+
+  useEffect(() => {
+    (async () => {
+      if (session && !(balance !== undefined)) {
+        const response = await fetch(`api/${session.user.id}/`, {
+          method: "POST",
+          body: JSON.stringify({
+            username: session.user.name,
+            email: session.user.email,
+          }),
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+        });
+        if (response.status === 200) {
+          const data = await response.json();
+          setBalance(data.accountBalance);
+        }
+      }
+    })();
+  }, [session]);
+
   const router = useRouter();
 
   const logOut = () => {
@@ -27,24 +51,30 @@ export default function Navbar({ user, balance }) {
   };
 
   return (
-    <div className={styles.navbar}>
-      <a onClick={() => handleNavClick("home")}>Mansfield</a>
-      <div className={styles.dropdown}>
-        <button type="button" className={styles.dropdownButton}>
-          {user} ▼
-        </button>
-        <div className={styles.dropdownContent}>
-          <a onClick={() => handleNavClick("bethistory")}>Bet History</a>
-          <a onClick={() => handleNavClick("openbets")}>Pending Bets</a>
-          <a onClick={() => handleNavClick("logout")}>Logout</a>
+    session && (
+      <div className={styles.navbar}>
+        <a onClick={() => handleNavClick("home")}>Mansfield</a>
+        <div className={styles.dropdown}>
+          <button type="button" className={styles.dropdownButton}>
+            {session.user.name} ▼
+          </button>
+          <div className={styles.dropdownContent}>
+            <a onClick={() => handleNavClick("bethistory")}>Bet History</a>
+            <a onClick={() => handleNavClick("openbets")}>Pending Bets</a>
+            <a onClick={() => handleNavClick("logout")}>Logout</a>
+          </div>
         </div>
+        <div className={styles.balance}>${balance}</div>
       </div>
-      <div className={styles.balance}>${balance}</div>
-    </div>
+    )
   );
 }
 
+Navbar.defaultProps = {
+  balance: undefined,
+};
+
 Navbar.propTypes = {
-  user: PropTypes.string.isRequired,
-  balance: PropTypes.number.isRequired,
+  balance: PropTypes.number,
+  setBalance: PropTypes.func.isRequired,
 };
