c0f73f4ddc735d9e30f16b9a0bc1c9f69fe0d0a7
Jack Greenburg <jack.greenburg7@gmail.com>
2023-12-03 20:24:50 -0500

    account creation api working, not fully implemented

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import PropTypes from "prop-types";
 import { useSession } from "next-auth/react";
+import { useState, useEffect } from "react";
 import styles from "@/styles/Home.module.css";
 import Navbar from "../components/Navbar";
 import Book from "../components/book/Book";
@@ -9,9 +10,32 @@ export default function Home({ setCurrentPending }) {
     required: true,
   });
 
-  const balance = 1000;
+  // const balance = 1000;
+  const [balance, setBalance] = useState();
 
-  return session ? (
+  useEffect(() => {
+    (async () => {
+      if (session && !(balance !== undefined)) {
+        const response = await fetch(`api/${session.user.id}/`, {
+          method: "POST",
+          body: JSON.stringify({
+            username: session.user.name,
+            email: session.user.email,
+          }),
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+        });
+        if (response.status === 200) {
+          const data = await response.json();
+          setBalance(data.accountBalance);
+        }
+      }
+    })();
+  }, [session]);
+
+  return balance !== undefined ? (
     <div className={styles.home}>
       <Navbar balance={balance} user={session.user.name} />
       <Book setCurrentPending={setCurrentPending} />
