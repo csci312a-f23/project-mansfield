a2a514751dca71e91db2b74892a9a6c026f0c875
Jack Greenburg <jack.greenburg7@gmail.com>
2023-12-12 17:38:05 -0500

    secured some important endpoints

--- src/pages/api/[id]/history.js
+++ src/pages/api/[id]/history.js
@@ -1,22 +1,27 @@
 // GET and POST bet history given the userId
 
-import {
-  getDatabase,
-  ref,
-  onValue,
-  set,
-  push,
-} from "firebase/database";
-
-
+import { getServerSession } from "next-auth";
+import { getDatabase, ref, onValue, set, push } from "firebase/database";
+import { authOptions } from "../auth/[...nextauth]";
 
 export default async function handler(req, res) {
- 
-  const { method, query} = req;
+  const session = await getServerSession(req, res, authOptions);
+  if (!session) {
+    res.status(401).json({ message: "You must be logged in." });
+    return;
+  }
+
+  const { method, query } = req;
+  if (session.user.id !== query.id) {
+    res
+      .status(401)
+      .json({ message: "You cannot access information of other users." });
+    return;
+  }
+
   const db = getDatabase();
   const historyRef = ref(db, `history/${query.id}`);
 
-  
   switch (method) {
     case "GET": {
       onValue(historyRef, (snapshot) => {
@@ -25,16 +30,14 @@ export default async function handler(req, res) {
       });
 
       break;
-
     }
 
     // push a new history entry into database
-    case "POST":{  
-        
+    case "POST": {
       const pushref = push(historyRef);
       set(pushref, req.body);
-  
-      res.status(200).json({"complete": true});
+
+      res.status(200).json({ complete: true });
       break;
     }
     default:
