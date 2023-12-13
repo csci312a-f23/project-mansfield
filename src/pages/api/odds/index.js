83a3128a758a854c328215ada19dee02d8d9cbce
Jack Greenburg <jack.greenburg7@gmail.com>
2023-12-12 21:57:00 -0500

    create separate scores table

--- src/pages/api/odds/index.js
+++ src/pages/api/odds/index.js
@@ -2,7 +2,7 @@ import { getDatabase, onValue, update, get, ref } from "firebase/database";
 
 async function fetchLeague(league) {
   const response = await fetch(
-    `https://api.the-odds-api.com//v4/sports/${league}/scores/?apiKey=${process.env.REACT_APP_ODDS_API_KEY}&regions=us&markets=h2h,spreads,totals&bookmakers=fanduel&dateFormat=unix&daysFrom=3`,
+    `https://api.the-odds-api.com//v4/sports/${league}/odds/?apiKey=${process.env.REACT_APP_ODDS_API_KEY}&regions=us&markets=h2h,spreads,totals&bookmakers=fanduel&dateFormat=unix`,
   );
   const games = await response.json();
   return games;
@@ -11,7 +11,7 @@ async function fetchLeague(league) {
 export default async function handler(req, res) {
   const { method } = req;
   const db = getDatabase();
-  const gamesRef = ref(db, `/scores`);
+  const gamesRef = ref(db, `/games`);
 
   //  Gets and updates this weeks scores the
   switch (method) {
@@ -24,7 +24,7 @@ export default async function handler(req, res) {
       break;
     }
     case "PUT": {
-      const refTime = ref(db, `scores/updated`);
+      const refTime = ref(db, `games/updated`);
       const updated = await get(refTime).then((snapshot) => snapshot.val());
 
       // set to update no more than every 45 minutes
