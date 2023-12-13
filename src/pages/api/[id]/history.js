fc013bd306ddbf01a602b862ea3b4bab7a493b7c
Jack Greenburg <jack.greenburg7@gmail.com>
2023-12-12 22:40:02 -0500

    idek

--- src/pages/api/[id]/history.js
+++ src/pages/api/[id]/history.js
@@ -1,24 +1,9 @@
 // GET and POST bet history given the userId
 
-import { getServerSession } from "next-auth";
 import { getDatabase, ref, onValue, set, push } from "firebase/database";
-import { authOptions } from "../auth/[...nextauth]";
 
 export default async function handler(req, res) {
-  const session = await getServerSession(req, res, authOptions);
-  if (!session) {
-    res.status(401).json({ message: "You must be logged in." });
-    return;
-  }
-
   const { method, query } = req;
-  if (session.user.id !== query.id) {
-    res
-      .status(401)
-      .json({ message: "You cannot access information of other users." });
-    return;
-  }
-
   const db = getDatabase();
   const historyRef = ref(db, `history/${query.id}`);
 
@@ -28,7 +13,6 @@ export default async function handler(req, res) {
         const data = snapshot.val();
         res.status(200).json(data);
       });
-
       break;
     }
 
